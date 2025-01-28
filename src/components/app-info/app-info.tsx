aa53b19fc89717b4884ceac7af42aabb627765ac
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-28 21:35:58 +0500

    Finished app-info

--- src/components/app-info/app-info.tsx
+++ src/components/app-info/app-info.tsx
@@ -3,7 +3,10 @@ import './app-info.css'
 
 const AppInfo = () => {
   return (
-    <div>AppInfo</div>
+    <div className="app-info">
+      <p className="fs-3 text-uppercase">All Movies:</p>
+      <p className="fs-4 text-uppercase">Favourite Movies:</p>
+    </div>
   )
 }
 
