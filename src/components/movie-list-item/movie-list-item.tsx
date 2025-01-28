f96d46e03284d092c6cf8085118a69e7ea87d7d3
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-28 22:37:46 +0500

    movies-list-item

---
+++ src/components/movie-list-item/movie-list-item.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './movie-list-item.css'
+
+const MovieListItem = () => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+        <span className="list-group-item-label">fff</span>
+        <input type="number" className="list-group-item-input" defaultValue="989" />
+        <div className="d-flex justify-content-center align-items-center">
+            <button className="btn-cookie btn-sm" type="button">
+                <i className="fas fa-cookie"></i>
+            </button>
+            <button className="btn-sm btn-trash" type="button">
+                <i className="fas fa-trash"></i>
+            </button>
+            <i className="fas fa-star"></i>
+        </div>
+    </li>
+  )
+}
+
+export default MovieListItem
--- src/components/movie-list/movie-list.tsx
+++ src/components/movie-list/movie-list.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './movie-list.css'
+import MovieListItem from '../movie-list-item/movie-list-item.tsx'
 
 const MovieList = () => {
   return (
-    <div className="movie-list">MovieList</div>
+    <div className="movie-list">
+        <MovieListItem />
+        <MovieListItem />
+        <MovieListItem />
+        <MovieListItem />
+    </div>
   )
 }
 
