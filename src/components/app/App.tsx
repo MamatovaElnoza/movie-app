276b55314a7e23459993230b7e5bfc43e0646aeb
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-29 16:35:34 +0500

    Creating a little server inside of the project

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,16 +7,24 @@ import MovieList from '../movie-list/movie-list.tsx';
 import MoviesAddForm from '../movies-add-form/movies-add-form.tsx';
 
 function App() {
+
+  const data = [
+    { name: 'Wednesday', views: 998 },
+    { name: 'Leon', views: 567 },
+    { name: 'Home Alone', views: 738 },
+    { name: 'The fault in our stars', views: 893 },
+  ]
+
   return (
-    <div className = "app font-monospace">
-      <div className = "content">
-      <AppInfo />
-      <div className= "search-panel">
-        <SearchPanel />
-        <AppFilter />
-      </div>
-      <MovieList />
-      <MoviesAddForm />
+    <div className="app font-monospace">
+      <div className="content">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <MovieList data={data} />
+        <MoviesAddForm />
       </div>
     </div>
   );
--- src/components/movie-list/movie-list.tsx
+++ src/components/movie-list/movie-list.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import './movie-list.css'
 import MovieListItem from '../movie-list-item/movie-list-item.tsx'
 
-const MovieList = () => {
+const MovieList = ({data}) => {
   return (
     <div className="movie-list">
-        <MovieListItem name='Wednesday' views='898'/>
-        <MovieListItem name='Leon' views='949'/>
-        <MovieListItem name='Home Alone' views='789'/>
-        <MovieListItem name='The fault in our stars' views='736'/>
+      {data.map(item =>(
+        <MovieListItem name={item.name} views={item.views}/>
+      ))}
     </div>
   )
 }
