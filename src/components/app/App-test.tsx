2d41c1bd6dd9498b763dad8fb3d236765b76aa51
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-29 21:40:35 +0500

    Input change linked to info

--- src/components/app/App-test.tsx
+++ src/components/app/App-test.tsx
@@ -8,30 +8,34 @@ interface UserProps {
 
 interface UserState {
     counter: number;
-  }
+}
 
 class User extends Component<UserProps, UserState> {
     constructor(props: UserProps) {
         super(props)
         this.state = {
-            counter: 10,
-            text : 'text'
+            counter: 10, age: ''
         }
     }
 
     clickIncrement = () => {
         this.setState(prevState => ({
-            counter: prevState.counter +1,
+            counter: prevState.counter + 1,
         }))
     }
     clickDecrement = () => {
         this.setState(prevState => ({
-            counter: prevState.counter -1,
+            counter: prevState.counter - 1,
         }))
     }
     clickRestart = () => {
         this.setState({
             counter: 0,
+        })
+    }
+    changeHandler = (e) =>{
+        this.setState({
+            age: e.target.value,
         })
     }
 
@@ -40,15 +44,18 @@ class User extends Component<UserProps, UserState> {
         return (
             <div className="w-50 mx-auto">
                 <div className="border p-3 mt-5">
-                    <h3>mening ismim - {firstname}, sharifim - {lastname}</h3>
+                    <h3>mening ismim - {firstname}, sharifim - {lastname}, yoshim - {this.state.age}</h3>
                     <a href={link}> youtube kanalim</a>
-                    <p>{this.state.text}</p>
                     <div className="pt-3">
                         <button onClick={this.clickIncrement} className="btn btn-success">increment</button>
                         <button onClick={this.clickDecrement} className="btn btn-danger mx-2">decrement</button>
                         <button onClick={this.clickRestart} className="btn btn-info">restart</button>
                     </div>
                     <p>{this.state.counter}</p>
+                    <form>
+                        <span>Yoshingiz</span>
+                        <input className="form-control" type="text" onChange={this.changeHandler} />
+                    </form>
                 </div>
             </div>
         )
