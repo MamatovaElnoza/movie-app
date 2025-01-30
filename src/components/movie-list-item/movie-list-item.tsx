ffc86192c632c58bb299cf606f707a2601c8e50d
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-30 09:51:02 +0500

    fixed

--- src/components/movie-list-item/movie-list-item.tsx
+++ src/components/movie-list-item/movie-list-item.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './movie-list-item.css'
 
-const MovieListItem = (props: { name: string; views: number; favourite: boolean; id: any }) => {
-  return (
-    <li className={`list-group-item d-flex justify-content-between ${props.favourite && 'favourite'}`}>
-        <span className="list-group-item-label">{props.name}</span>
-        <input type="number" className="list-group-item-input" defaultValue={props.views} />
-        <div className="d-flex justify-content-center align-items-center">
-            <button className="btn-cookie btn-sm" type="button">
-                <i className="fas fa-cookie"></i>
-            </button>
-            <button className="btn-sm btn-trash" type="button">
-                <i className="fas fa-trash"></i>
-            </button>
-            <i className="fas fa-star"></i>
-        </div>
-    </li>
-  )
+interface MovieProps {
+  name: string;
+  views: number;
+  favourite: boolean;
+}
+
+class MovieListItem extends Component<MovieProps>{
+  
+  render() {
+    const { name, views, favourite} = this.props;
+    return (
+      <li className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'}`}>
+          <span className="list-group-item-label">{name}</span>
+          <input type="number" className="list-group-item-input" defaultValue={views} />
+          <div className="d-flex justify-content-center align-items-center">
+              <button className="btn-cookie btn-sm" type="button">
+                  <i className="fas fa-cookie"></i>
+              </button>
+              <button className="btn-sm btn-trash" type="button">
+                  <i className="fas fa-trash"></i>
+              </button>
+              <i className="fas fa-star"></i>
+          </div>
+      </li>
+    )
+  }
 }
 
 export default MovieListItem
