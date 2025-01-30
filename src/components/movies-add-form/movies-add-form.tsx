35489134e9e058e1135f10fdbf943db90039f93f
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-30 11:41:22 +0500

    Target.name to the state

--- src/components/movies-add-form/movies-add-form.tsx
+++ src/components/movies-add-form/movies-add-form.tsx
@@ -15,13 +15,19 @@ class MoviesAddForm extends Component<MoviesAddProps> {
     }
   }
 
+  changeHandlerInput = (e: any) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div className="movies-add-form">
         <h3>Add new movie</h3>
         <form className="add-form d-flex">
-          <input type="text" className="form-control new-post-label" placeholder="Movie name?" />
-          <input type="number" className="form-control new-post-label" placeholder="Number of times watched?" />
+          <input type="text" className="form-control new-post-label" placeholder="Movie name?" onChange={this.changeHandlerInput} name='name'/>
+          <input type="number" className="form-control new-post-label" placeholder="Number of times watched?" onChange={this.changeHandlerInput} name='views'/>
           <button type="submit" className="btn btn-outline-dark">Add</button>
         </form>
       </div>
