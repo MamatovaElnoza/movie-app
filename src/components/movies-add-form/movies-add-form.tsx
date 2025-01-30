c716ffc933e92e0ad300bd384a89ab2011a38035
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-30 11:14:03 +0500

    Altering movies-add-form to class component

--- src/components/movies-add-form/movies-add-form.tsx
+++ src/components/movies-add-form/movies-add-form.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './movies-add-form.css'
 
-const MoviesAddForm = () => {
-  return (
-    <div className="movies-add-form">
+class MoviesAddForm extends Component {
+  render() {
+    return (
+      <div className="movies-add-form">
         <h3>Add new movie</h3>
         <form className="add-form d-flex">
-            <input type="text" className="form-control new-post-label" placeholder="Movie name?" />
-            <input type="number" className="form-control new-post-label" placeholder="Number of times watched?" />
-            <button type="submit" className="btn btn-outline-dark">Add</button>
+          <input type="text" className="form-control new-post-label" placeholder="Movie name?" />
+          <input type="number" className="form-control new-post-label" placeholder="Number of times watched?" />
+          <button type="submit" className="btn btn-outline-dark">Add</button>
         </form>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default MoviesAddForm
