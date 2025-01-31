1f51596d87c4224bc12f9944902dee1d6cce1f9d
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-31 18:21:54 +0500

    explicitly defining the state type

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css'
 import AppInfo from '../app-info/app-info.tsx';
 import SearchPanel from '../search-panel/search-panel.tsx';
@@ -6,32 +6,35 @@ import AppFilter from '../app-filter/app-filter.tsx';
 import MovieList from '../movie-list/movie-list.tsx';
 import MoviesAddForm from '../movies-add-form/movies-add-form.tsx';
 
-class App extends Component {
-  constructor(props){
+interface AppMovie {
+  id: number;
+  name: string;
+  views: number;
+  favourite: boolean;
+}
+interface AppState {
+  data: AppMovie[];
+}
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
     super(props)
     this.state = {
       data: [
-    { name: 'Wednesday', views: 998, favourite: false, id: 1 },
-    { name: 'Leon', views: 567, favourite: false, id: 2 },
-    { name: 'Home Alone', views: 738, favourite: true, id: 3 },
-    { name: 'The fault in our stars', views: 893, favourite: false, id: 4 },
-  ]
+        { name: 'Wednesday', views: 998, favourite: false, id: 1 },
+        { name: 'Leon', views: 567, favourite: false, id: 2 },
+        { name: 'Home Alone', views: 738, favourite: true, id: 3 },
+        { name: 'The fault in our stars', views: 893, favourite: false, id: 4 },
+      ]
     }
   }
 
-  onDelete = id => {
-    this.setState(({data}) => {
-      const index = data.findIndex(c => c.id === id)
-      data.splice(index, 1)
-      return{
-        data,
-      }
-    })
-    
+  onDelete = (id: number) => {
+    this.setState(({ data }) => ({ data: data.filter(c => c.id !== id), }))
+
   }
 
-  render(){
-    const {data} = this.state
+  render() {
+    const { data } = this.state
     return (
       <div className="app font-monospace">
         <div className="content">
@@ -40,7 +43,7 @@ class App extends Component {
             <SearchPanel />
             <AppFilter />
           </div>
-          <MovieList data={data} onDelete={this.onDelete}/>
+          <MovieList data={data} onDelete={this.onDelete} />
           <MoviesAddForm name='' views={0} />
         </div>
       </div>
