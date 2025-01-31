4e464a5cc4d8f4ea9625c2e9ed731ee2151eb640
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-01-31 17:52:09 +0500

    converting app to class component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css'
 import AppInfo from '../app-info/app-info.tsx';
 import SearchPanel from '../search-panel/search-panel.tsx';
@@ -6,33 +6,40 @@ import AppFilter from '../app-filter/app-filter.tsx';
 import MovieList from '../movie-list/movie-list.tsx';
 import MoviesAddForm from '../movies-add-form/movies-add-form.tsx';
 
-function App() {
-
-  const data: { name: string; views: number; favourite: boolean, id: any}[] = [
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
     { name: 'Wednesday', views: 998, favourite: false, id: 1 },
     { name: 'Leon', views: 567, favourite: false, id: 2 },
     { name: 'Home Alone', views: 738, favourite: true, id: 3 },
     { name: 'The fault in our stars', views: 893, favourite: false, id: 4 },
   ]
+    }
+  }
 
-  const onDelete = id => {
+  onDelete = id => {
     console.log(id);
     
   }
 
-  return (
-    <div className="app font-monospace">
-      <div className="content">
-        <AppInfo />
-        <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+  render(){
+    const {data} = this.state
+    return (
+      <div className="app font-monospace">
+        <div className="content">
+          <AppInfo />
+          <div className="search-panel">
+            <SearchPanel />
+            <AppFilter />
+          </div>
+          <MovieList data={data} onDelete={this.onDelete}/>
+          <MoviesAddForm name='' views={0} />
         </div>
-        <MovieList data={data} onDelete={onDelete}/>
-        <MoviesAddForm name='' views={0} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
