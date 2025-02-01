66dd3ffa2711cf2c0450168a859b0e759e5dce46
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-02-02 00:13:49 +0500

    improved

--- src/components/movie-list-item/movie-list-item.tsx
+++ src/components/movie-list-item/movie-list-item.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './movie-list-item.css'
 
 const MovieListItem = props => {
-	const { name, viewers, onDelete, onToggleProp, favourite, like } = props
+	const { name, views, onDelete, onToggleProp, favourite, like } = props
 
 	return (
 		<li className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'} ${like && 'like'}`}>
 			<span onClick={onToggleProp} className='list-group-item-label' data-toggle='like'>
 				{name}
 			</span>
-			<input type='number' className='list-group-item-input' defaultValue={viewers} />
+			<input type='number' className='list-group-item-input' defaultValue={views} />
 			<div className='d-flex justify-content-center align-items-center'>
 				<button type='button' className='btn-cookie btn-sm' onClick={onToggleProp} data-toggle='favourite'>
 					<i className='fas fa-cookie'></i>
