7dbc3e847b6003a761e5304080c5486cdd6ab4c1
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-02-03 23:42:08 +0500

    Favourite Movies Count with filter.length

--- src/components/app-info/app-info.tsx
+++ src/components/app-info/app-info.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './app-info.css'
 
-const AppInfo = ({allMoviesCount}) => {
+const AppInfo = ({allMoviesCount, favouriteMovieCount}) => {
   return (
     <div className="app-info">
       <p className="fs-3 text-uppercase">All Movies: {allMoviesCount}</p>
-      <p className="fs-4 text-uppercase">Favourite Movies:</p>
+      <p className="fs-4 text-uppercase">Favourite Movies: {favouriteMovieCount}</p>
     </div>
   )
 }
