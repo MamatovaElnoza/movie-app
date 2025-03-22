67996f9e88dbc8db030d598695c6e6165fad9884
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-03-23 03:47:05 +0500

    altering the whole project to function component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import AppInfo from '../app-info/app-info.tsx';
 import SearchPanel from '../search-panel/search-panel.tsx';
@@ -7,64 +7,59 @@ import MovieList from '../movie-list/movie-list.tsx';
 import MoviesAddForm from '../movies-add-form/movies-add-form.tsx';
 import { v4 as uuidv4 } from 'uuid';
 
-interface AppMovie {
-  [x: string]: any;
-  id: string | number;
-  name: string;
-  views: number;
-  favourite: boolean;
-  like: boolean;
-}
-interface AppState {
-  data: AppMovie[];
-  term: string;
-  filter: string;
-}
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props)
-    this.state = {
-      data: [
-        { name: 'Wednesday', views: 998, favourite: false, like: false, id: 1 },
-        { name: 'Leon', views: 567, favourite: false, like: false, id: 2 },
-        { name: 'Home Alone', views: 738, favourite: false, like: false, id: 3 },
-        { name: 'The fault in our stars', views: 893, favourite: false, like: false, id: 4 },
-      ],
-      term: '',
-      filter: 'all',
-    }
+// interface AppMovie {
+//   [x: string]: any;
+//   id: string | number;
+//   name: string;
+//   views: number;
+//   favourite: boolean;
+//   like: boolean;
+// }
+// interface AppState {
+//   data: AppMovie[];
+//   term: string;
+//   filter: string;
+// }
+
+const App = () => {
+  const [data, setData] = useState([
+    { name: 'Wednesday', views: 998, favourite: false, like: false, id: 1 },
+    { name: 'Leon', views: 567, favourite: false, like: false, id: 2 },
+    { name: 'Home Alone', views: 738, favourite: false, like: false, id: 3 },
+    { name: 'The fault in our stars', views: 893, favourite: false, like: false, id: 4 },
+  ]);
+  const [term, setTerm] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  const onDelete = (id) => {
+    const newArr = data.filter(c => c.id !== id)
+    setData(newArr)
   }
 
-  onDelete = (id: number) => {
-    this.setState(({ data }) => ({ data: data.filter(c => c.id !== id), }))
-  }
-
-  addForm = (e, item) => {
+  const addForm = (item) => {
     const newItem = { name: item.name, views: item.views, id: uuidv4(), favourite: false, like: false }
-    this.setState(({ data }) => ({
-      data: [...data, newItem]
-    }))
+    const newArr = [...data, newItem]
+    setData(newArr)
   }
 
-  onToggleProp = (id, prop) => {
-    this.setState(({ data }) => ({
-      data: data.map(item => {
-        if (item.id === id) {
-          return { ...item, [prop]: !item[prop] }
-        }
-        return item
-      })
-    }))
+  const onToggleProp = (id, prop) => {
+    const newArr = data.map(item => {
+      if (item.id === id) {
+        return { ...item, [prop]: !item[prop] }
+      }
+      return item
+    })
+    setData(newArr)
   }
 
-  searchHandler = (arr, term) => {
-    if (term.length === 0) {
+  const searchHandler = (arr, term) => {
+    if (term === 0) {
       return arr
     }
     return arr.filter(item => item.name.toLowerCase().indexOf(term) > -1)
   }
 
-  filterHandler = (arr, filter) => {
+  const filterHandler = (arr, filter) => {
     switch (filter) {
       case 'popular':
         return arr.filter(c => c.like)
@@ -77,29 +72,23 @@ class App extends Component<{}, AppState> {
     }
   }
 
-  updateTermHandler = (term) => this.setState({ term })
-  updateFilterHandler = (filter) => this.setState({ filter })
+  const updateTermHandler = term => setTerm(term)
 
-  render() {
-    const { data, term, filter } = this.state
-    const allMoviesCount = data.length
-    const favouriteMovieCount = data.filter(c => c.favourite).length
-    const visibleData = this.filterHandler(this.searchHandler(data, term), filter)
+  const updateFilterHandler = filter => setFilter(filter)
 
-    return (
-      <div className="app font-monospace">
-        <div className="content">
-          <AppInfo allMoviesCount={allMoviesCount} favouriteMovieCount={favouriteMovieCount} />
-          <div className="search-panel">
-            <SearchPanel updateTermHandler={this.updateTermHandler} />
-            <AppFilter filter={filter} updateFilterHandler={this.updateFilterHandler}/>
-          </div>
-          <MovieList onToggleProp={this.onToggleProp} data={visibleData} onDelete={this.onDelete} />
-          <MoviesAddForm addForm={this.addForm} />
+  return (
+    <div className="app font-monospace">
+      <div className="content">
+        <AppInfo allMoviesCount={data.length} favouriteMovieCount={data.filter(c => c.favourite).length} />
+        <div className="search-panel">
+          <SearchPanel updateTermHandler={updateTermHandler} />
+          <AppFilter filter={filter} updateFilterHandler={updateFilterHandler}/>
         </div>
+        <MovieList onToggleProp={onToggleProp} data={filterHandler(searchHandler(data, term), filter)} onDelete={onDelete} />
+        <MoviesAddForm addForm={addForm} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
