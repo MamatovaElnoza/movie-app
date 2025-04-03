1a31cbddabdcc9ab7a59052b71edf88f61e9fce5
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-04-04 00:15:42 +0500

    component fully functioned

--- src/components/movies-add-form/movies-add-form.tsx
+++ src/components/movies-add-form/movies-add-form.tsx
@@ -1,67 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './movies-add-form.css';
 
-interface MoviesAddProps {
-  addForm: (e: React.FormEvent, item: { name: string; views: number }) => void;
-}
+// interface MoviesAddProps {
+//   addForm: (e: React.FormEvent, item: { name: string; views: number }) => void;
+// }
 
-interface MoviesAddState {
-  name: string;
-  views: string;
-}
+// interface MoviesAddState {
+//   name: string;
+//   views: string;
+// }
 
-class MoviesAddForm extends Component<MoviesAddProps, MoviesAddState> {
-  constructor(props: MoviesAddProps) {
-    super(props);
-    this.state = {
-      name: '',
-      views: '',
-    };
+const MoviesAddForm = ({addForm}) => {
+  const [state, setState] = useState({ name: '', views: '' })
+
+  const changeHandlerInput = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value, })
   }
 
-  changeHandlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    } as Pick<MoviesAddState, keyof MoviesAddState>);
-  };
-
-  onSubmit = (e: React.FormEvent) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    const { name, views } = this.state;
-    if (name.trim() && views.trim()) {
-      this.props.addForm(e, { name, views: Number(views) });
-      this.setState({ name: '', views: '' }); // Reset form after submit
-    }
-  };
-
-  render() {
-    const { name, views } = this.state;
-
-    return (
-      <div className="movies-add-form">
-        <h3>Add new movie</h3>
-        <form className="add-form d-flex" onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            className="form-control new-post-label"
-            placeholder="Movie name?"
-            onChange={this.changeHandlerInput}
-            name="name"
-            value={name}
-          />
-          <input
-            type="number"
-            className="form-control new-post-label"
-            placeholder="Number of times watched?"
-            onChange={this.changeHandlerInput}
-            name="views"
-            value={views}
-          />
-          <button type="submit" className="btn btn-outline-dark">Add</button>
-        </form>
-      </div>
-    );
+    const data = {name: state.name, viewers: state.views}
+    addForm(data)
+    setState({name: '', views: ''})
   }
+
+  return (
+    <div className="movies-add-form">
+      <h3>Add new movie</h3>
+      <form className="add-form d-flex" onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="form-control new-post-label"
+          placeholder="Movie name?"
+          onChange={changeHandlerInput}
+          name="name"
+          value={state.name}
+        />
+        <input
+          type="number"
+          className="form-control new-post-label"
+          placeholder="Number of times watched?"
+          onChange={changeHandlerInput}
+          name="views"
+          value={state.views}
+        />
+        <button type="submit" className="btn btn-outline-dark">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default MoviesAddForm;
